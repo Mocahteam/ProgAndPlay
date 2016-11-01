0dead6e3a886cebe36658a945ce7bd0068cf346b
muratet <mathieu.muratet@lip6.fr>
2016-11-01 01:37:05 +0100

    Add Scratch 2.0 fork

---
+++ Client_Interfaces/scratch2.0/ProgAndPlayANE/lib/exportANE/ProgAndPlayExtension.cpp
@@ -0,0 +1,132 @@
+#include "FlashRuntimeExtensions.h"
+#include "PP_Client.h"
+#include "stdlib.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ProgAndPlayContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, uint32_t* numFunctionsToSet, const FRENamedFunction** functionsToSet);
+void ProgAndPlayContextFinalizer(FREContext ctx);
+
+FREObject FRE_PP_Open(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]);
+FREObject FRE_PP_Close(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]);
+
+FREObject FRE_PP_IsGameOver(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]);
+FREObject FRE_PP_MapWidth(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]);
+FREObject FRE_PP_MapHeight(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]);
+FREObject FRE_PP_StartPosX(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]);
+FREObject FRE_PP_StartPosY(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]);
+FREObject FRE_PP_NumSpecialArea(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]);
+FREObject FRE_PP_SpecialAreaPosX(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]);
+FREObject FRE_PP_SpecialAreaPosY(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]);
+
+void ProgAndPlayInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet) {
+	extDataToSet = 0;  // pas de données commune au contexte dans notre cas. 
+	*ctxInitializerToSet = &ProgAndPlayContextInitializer;
+	*ctxFinalizerToSet = &ProgAndPlayContextFinalizer;
+}
+
+void ProgAndPlayFinalizer(void* extData) {
+	// Rien à faire
+	return;
+}
+
+void ProgAndPlayContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, 
+                        uint32_t* numFunctionsToSet, 
+                        const FRENamedFunction** functionsToSet) { 
+	static FRENamedFunction extensionFunctions[] =
+	{
+		{ (const uint8_t*) "PP_Open_wrapper",        0, &FRE_PP_Open },
+		{ (const uint8_t*) "PP_Close_wrapper",        0, &FRE_PP_Close },
+		{ (const uint8_t*) "PP_IsGameOver_wrapper",        0, &FRE_PP_IsGameOver },
+		{ (const uint8_t*) "PP_MapWidth_wrapper",        0, &FRE_PP_MapWidth },
+		{ (const uint8_t*) "PP_MapHeight_wrapper",        0, &FRE_PP_MapHeight },
+		{ (const uint8_t*) "PP_StartPosX_wrapper",        0, &FRE_PP_StartPosX },
+		{ (const uint8_t*) "PP_StartPosY_wrapper",        0, &FRE_PP_StartPosY },
+		{ (const uint8_t*) "PP_NumSpecialArea_wrapper",        0, &FRE_PP_NumSpecialArea },
+		{ (const uint8_t*) "PP_SpecialAreaPosX_wrapper",        0, &FRE_PP_SpecialAreaPosX },
+		{ (const uint8_t*) "PP_SpecialAreaPosY_wrapper",        0, &FRE_PP_SpecialAreaPosY }
+	};
+
+	// Tell AIR how many functions there are in the array:
+	*numFunctionsToSet = sizeof(extensionFunctions) / sizeof(FRENamedFunction);
+
+	// Set the output parameter to point to the array we filled in:
+	*functionsToSet = extensionFunctions;
+}
+
+void ProgAndPlayContextFinalizer(FREContext ctx){
+	// Rien à faire dans notre cas
+}
+
+FREObject FRE_PP_Open(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]){
+	PP_Open();
+	return 0;
+}
+
+FREObject FRE_PP_Close(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]){
+	PP_Close();
+	return 0;
+}
+
+FREObject FRE_PP_IsGameOver(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]){
+	FREObject value;
+	int isGameOver = PP_IsGameOver() > 0 ? 1 : 0;
+	FRENewObjectFromBool(isGameOver, &value);
+	return value;
+}
+
+FREObject FRE_PP_MapWidth(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]){
+	FREObject value;
+	FRENewObjectFromInt32(PP_GetMapSize().x, &value);
+	return value;
+}
+
+FREObject FRE_PP_MapHeight(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]){
+	FREObject value;
+	FRENewObjectFromInt32(PP_GetMapSize().y, &value);
+	return value;
+}
+
+FREObject FRE_PP_StartPosX(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]){
+	FREObject value;
+	FRENewObjectFromDouble(PP_GetStartPosition().x, &value);
+	return value;
+}
+
+FREObject FRE_PP_StartPosY(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]){
+	FREObject value;
+	FRENewObjectFromDouble(PP_GetStartPosition().y, &value);
+	return value;
+}
+
+FREObject FRE_PP_NumSpecialArea(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]){
+	FREObject value;
+	FRENewObjectFromInt32(PP_GetNumSpecialAreas(), &value);
+	return value;
+}
+
+FREObject FRE_PP_SpecialAreaPosX(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]){
+	// Récupération du paramètre
+	int num;
+	FREGetObjectAsInt32(argv[0], &num);
+	// Appel Prog&Play pour la valeur de retour 
+	FREObject value;
+	FRENewObjectFromDouble(PP_GetSpecialAreaPosition(num).x, &value);
+	return value;
+}
+
+FREObject FRE_PP_SpecialAreaPosY(FREContext ctx, void* functionData, uint32_t argc, FREObject argv[]){
+	// Récupération du paramètre
+	int num;
+	FREGetObjectAsInt32(argv[0], &num);
+	// Appel Prog&Play pour la valeur de retour 
+	FREObject value;
+	FRENewObjectFromDouble(PP_GetSpecialAreaPosition(num).y, &value);
+	return value;
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Client_Interfaces/scratch2.0/ProgAndPlayANE/src/ProgAndPlayANE.as
@@ -0,0 +1,83 @@
+package
+{
+
+	import flash.events.EventDispatcher;
+	import flash.external.ExtensionContext;
+	/**
+	 * ...
+	 * @author MM
+	 */
+	public class ProgAndPlayANE extends EventDispatcher
+	{
+		
+		private var progPlayContext:ExtensionContext = null;
+		
+		public function ProgAndPlayANE() 
+		{
+			trace ("Windows : Tentative d'ouverture de l'extension de nom ProgAndPlayExtension");
+			progPlayContext = ExtensionContext.createExtensionContext("ProgAndPlayExtension", "");
+			if (progPlayContext == null)
+				trace ("Extension non chargée...");
+			else
+				trace ("Extension chargée (YES!!!) ...");
+			// Ouverture du PP_Open
+			if (progPlayContext != null)
+				progPlayContext.call("PP_Open_wrapper");
+		}
+		
+		public function PP_IsGameOver_ext():Boolean{
+			if (progPlayContext != null)
+				return Boolean (progPlayContext.call("PP_IsGameOver_wrapper"));
+			return false;
+		}
+		
+		public function PP_MapWidth_ext():int{
+			if (progPlayContext != null)
+				return int (progPlayContext.call("PP_MapWidth_wrapper"));
+			return -1;
+		}
+		
+		public function PP_MapHeight_ext():int{
+			if (progPlayContext != null)
+				return int (progPlayContext.call("PP_MapHeight_wrapper"));
+			return -1;
+		}
+		
+		public function PP_StartPosX_ext():Number{
+			if (progPlayContext != null)
+				return Number (progPlayContext.call("PP_StartPosX_wrapper"));
+			return -1;
+		}
+		
+		public function PP_StartPosY_ext():Number{
+			if (progPlayContext != null)
+				return Number (progPlayContext.call("PP_StartPosY_wrapper"));
+			return -1;
+		}
+		
+		public function PP_NumSpecialArea_ext():int{
+			if (progPlayContext != null)
+				return int (progPlayContext.call("PP_NumSpecialArea_wrapper"));
+			return -1;
+		}
+		
+		public function PP_SpecialAreaPosX_ext(num:int):int{
+			if (progPlayContext != null)
+				return int (progPlayContext.call("PP_SpecialAreaPosX_wrapper", num));
+			return -1;
+		}
+		
+		public function PP_SpecialAreaPosY_ext(num:int):int{
+			if (progPlayContext != null)
+				return int (progPlayContext.call("PP_SpecialAreaPosY_wrapper", num));
+			return -1;
+		}
+		
+		public function PP_Close_ext():void{
+			if (progPlayContext != null){
+				progPlayContext.call("PP_Close_wrapper");
+				progPlayContext = null;
+			}
+		}
+	}
+}
